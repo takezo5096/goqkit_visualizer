5c3bf2753a4640a07ff91f398e67e93de12bb08c
takezo5096 <sevenstars5096@gmail.com>
2021-02-19 10:08:09 +0900

    remove a comment

--- goqkit_visualizer.js
+++ goqkit_visualizer.js
@@ -226,7 +226,6 @@ function draw_circuit(test_json) {
         var op = test_json.operations[x]
 
         if (op.op_name == "Sp"){
-            console.log("Sp")
             current_x++
             hit_qbits_array = {}
             hit_qbits = false
